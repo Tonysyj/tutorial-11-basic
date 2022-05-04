bd082f340cb9a007f6d241e9577394a634aa8992
Tonysyj <sunyuejun8623@gmail.com>
2022-05-05 01:02:51 +1000

    first homework commit

---
+++ python/homeworks/Python-Homework1.py
@@ -0,0 +1,25 @@
+from unittest import result
+
+
+person=['Jack', 'Tom', 'Lucy', 'Amy'] 
+print(person)
+person.append('Tony')
+print(person)
+person.remove('Tom')
+print(person)
+person.pop(0)
+print(person)
+person.insert(0,'Stephen')
+print(person)
+
+student={'name':"Tony", 'major':"HR", 'age':30, 'height':"176cm"}
+print(student)
+student_gender={'gender':"male"}
+print(student)
+result=student.pop('major')
+print(student)
+print(result)
+x=student.keys()
+print(x)
+y=student.values()
+print(y)
