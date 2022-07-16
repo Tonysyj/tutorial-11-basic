f3ce7a522ed0d7de0fcd2e15fab1429eeac64b59
Tonysyj <sunyuejun8623@gmail.com>
2022-07-16 16:33:37 +1000

    project1 front end complete

--- Project 1/src/script.js
+++ Project 1/src/script.js
@@ -1,1 +1,7 @@
-console.log("Hello World");
+const menuToggle = document.querySelector(".toggle"); /*寻找class toggle*/
+const showcase = document.querySelector(".showcase"); /*寻找class showcase*/
+
+menuToggle.addEventListener("click", () => {
+  menuToggle.classList.toggle("active");
+  showcase.classList.toggle("active");
+});
